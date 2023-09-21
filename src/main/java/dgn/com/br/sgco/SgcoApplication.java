b0df2a6556b44d4acc7f5f6ca64efbc44472264c
Your Name <you@example.com>
2023-09-21 13:50:47 -0300

    init

---
+++ src/main/java/dgn/com/br/sgco/SgcoApplication.java
@@ -0,0 +1,13 @@
+package dgn.com.br.sgco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgn/com/br/sgco/SgcoApplicationTests.java
@@ -0,0 +1,13 @@
+package dgn.com.br.sgco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
