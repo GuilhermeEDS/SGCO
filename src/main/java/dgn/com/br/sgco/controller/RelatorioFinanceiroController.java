30c1810fc29abdef9115f510872ea396848e868d
GuilhermeEDS <102765171+GuilhermeEDS@users.noreply.github.com>
2023-12-06 21:36:07 -0300

    Adição de download da planilha de dados

--- src/main/java/dgn/com/br/sgco/controller/RelatorioFinanceiroController.java
+++ src/main/java/dgn/com/br/sgco/controller/RelatorioFinanceiroController.java
@@ -1,16 +1,25 @@
 package dgn.com.br.sgco.controller;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import dgn.com.br.sgco.service.AgendamentoService;
 import lombok.NonNull;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RelatorioFinanceiroController {
@@ -24,5 +33,23 @@ public class RelatorioFinanceiroController {
 
         return "relatorioFinanceiro";
     }
-    
+
+    @RequestMapping(value = "/relatorio-financeiro/relatorio", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> dadosFinanceirosExcel() {
+        Map<String, Float[]> mesesValores = agendamentoService.porMes();
+        StringBuilder relatorioCsv = new StringBuilder("Mês; Acompanhamentos; Cirurgias; Consultas; Rendimentos\n");
+
+        mesesValores.entrySet().stream()
+                .map((mes) -> mes.getKey() + "; " + mes.getValue()[0] + "; " + mes.getValue()[1] + "; " + mes.getValue()[2] + "; " + mes.getValue()[3] + "\n")
+                .forEach(relatorioCsv::append);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "text/csv");
+        responseHeaders.set("Content-Encoding", "UTF-8");
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(relatorioCsv.toString());
+    }
 }
